8b622d18462888f8339f86f1aa39e8d8fdc5080f
idimad <idimad10@gmail.com>
2020-10-18 10:46:38 +0200

    article_LeMonde.py

--- article_LeMonde.py
+++ article_LeMonde.py
@@ -2,16 +2,15 @@ import requests
 import bs4
 #Import BeautifulSoup 
 from bs4 import BeautifulSoup
+import pandas as pd
 
 #def recup(domaine):
     
-requete = requests.get("https://www.lemonde.fr/economie-francaise/") 
+requete = requests.get("https://www.lemonde.fr/entreprises/") 
             
 page = requete.content
 soup = bs4.BeautifulSoup(page, 'lxml')
 
-print(soup.prettify())
-              
   
 url = soup.find_all("a",attrs={"class":'teaser__link'})
 
@@ -24,65 +23,67 @@ listTitre = []
 for a in url :
     listeUrl.append(a.get('href'))
     
-    if len(listeUrl) ==5 :
+    if len(listeUrl) ==100 :
         break
     
 for a in range(len(titre)) :
     listTitre.append(titre[a].text)
         
-    if len(listTitre) ==5 :
+    if len(listTitre) ==100 :
         break
 
 
-pageArticle =[]
+pageArticleTot =[]
+auteurTot=[]
 
 for i in range(len(listeUrl)):
     paragrapheTot=[]
-    titreTot=[]
+    
     requeteUrl = requests.get(listeUrl[i])
     pageUrl = requeteUrl.content
     soupUrl = bs4.BeautifulSoup(pageUrl, 'lxml')
     paragraphe = soupUrl.find_all("p",attrs={"class":'article__paragraph'})
     
-    titre = soupUrl.find_all("h3",attrs={"class":'article__title'})
+    auteur = soupUrl.find("meta", {"property": "og:article:author"})
+    auteur = auteur['content'] if auteur else 'Inconnu'
+        
+    auteurTot.append(auteur)
+    
     for i in paragraphe:
         paragrapheTot.append(i.text)
-        titreTot.append(titre)
-        
-    pageArticle.append(paragrapheTot)
+            
+    pageArticleTot.append(paragrapheTot)
+
+
+df = pd.DataFrame({'Auteur':auteurTot,'Url':listeUrl ,'Titre':listTitre,'Texte':pageArticleTot})
+    
+df.to_csv("C:\\Users\\idel\\Desktop\\M2\\Python\\LeMonde.csv",index=False)
 
 
 
-pageArticle
-
-
-for i in range(len(pageArticle)):
-    globals()["Article_%s"%i]= ''.join(pageArticle[i])    
-
+for i in range(len(pageArticleTot)):
+    globals()["Article_%s"%i]= ''.join(pageArticleTot[i])    
 
 
 
 
 #NLP
 
-pip install spacy
 
-python3 -m spacy download fr_core_web_sm
+#Intallation
+#pip install --upgrade pip
+#pip install spacy   
+#pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.1.0/en_core_web_sm-2.1.0.tar.gz
+#pip install https://github.com/explosion/spacy-models/releases/download/fr_core_news_sm-2.1.0/fr_core_news_sm-2.1.0.tar.gz
 
-import spacy
-import fr_core_web_sm
+#conda install -c conda-forge spacy-model-fr_core_news_sm
 
-nlp = spacy.load("fr_core_news_sm")
-
-
-def return_token(sentence):
-    # Tokeniser la phrase
-    doc = nlp(sentence)
-    # Retourner le texte de chaque token
-    return [X.text for X in doc]
+#python -m spacy download en_core_web_sm
+#python -m  spacy download fr_core_web_sm
 
 
 
-#return url
-            
-#recup("economie-francaise")
+
+
+
+    

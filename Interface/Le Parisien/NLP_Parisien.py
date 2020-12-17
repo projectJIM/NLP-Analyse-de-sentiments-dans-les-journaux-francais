46121c5bc6b945019228d8c49c70f9f6baa9849d
Jovan Stojanovic <jovanst98@yahoo.fr>
2020-12-17 20:32:00 +0100

    Rajout des donnees et de la partie NLP

---
+++ Interface/Le Parisien/NLP_Parisien.py
@@ -0,0 +1,320 @@
+import pandas as pd
+import nltk
+#nltk.download()
+
+
+##################################################################################
+###############  LE PARISIEN: LINKED COMPANIES AND INDIVIDUALS  ##################
+##################################################################################
+
+org_lvmh=['LVMH', 'Louis Vuitton','Ao Yun','Ardbeg','Belvedere','Bodega Numanthia',
+'Cape Mentelle','Chandon','Château Cheval Blanc',"Château d'Yquem","Cloudy Bay","Dom Pérignon",
+"Glenmorangie","Hennessy","Krug","Mercier","Moët & Chandon","Newton Vineyard",
+"Ruinart","Terrazas de los Andes","Veuve Clicquot","Volcan de mi tierra",
+"Woodinville","Wenjun","Berluti","Céline","Charles & Keith","Christian Dior",
+"Deus Ex Machina","Emilio Pucci","Fendi","Fenty","Givenchy","Kenzo","Loewe",
+"Loro Piana","Marc Jacobs","Moynat","Nicholas Kirkwood","Patou","Rimowa",
+"R. M. Williams","Thomas Pink","Acqua di Parma","Benefit Cosmetics","Fresh",
+"Givenchy Parfums","Guerlain","Kendo Brands","Bite Beauty",
+"Fenty Beauty by Rihanna", "Marc Jacobs Beauty","KVD Vegan Beauty",
+"Kat Von D Beauty","OleHenriksen","Ole Henriksen","Kenzo Parfums","Maison Francis Kurkdjian",
+"Make Up For Ever","Christian Dior","Loewe","Bulgari","Chaumet","FRED",
+"Hublot","TAG Heuer","Zenith","DFS","La Grande Epicerie","Le Bon Marché","Sephora",
+"Starboard Cruise Services","Belmond", "Caffè-Pasticceria Cova","Cheval Blanc",
+"Royal Van Lent Shipyard","Princess Yachts","Les Echos","Pinarello",'Bernard Arnault',
+'Radio Classique','Investir - Le Journal des Finances','la Samaritaine','Bvlgari','Celine']
+
+
+org_conc=['Chanel','Hermes','Gucci','Burberry','Cartier','Prada','Rémy Martin','Pernod Ricard',
+          'Martell','Chivas Regal','Absolut Vodka','Ballantines','Courvoisier']
+
+
+##################################################################################
+#########  1. Importing the corpora with news articles ###########################
+##################################################################################
+
+df=pd.read_csv('C:/Users/jovan/OneDrive/Radna površina/Paris 1/NLP_2/data/le_parisien/le_parisien_final.csv')
+
+# 13 194 articles du parisien
+
+##################################################################################
+######################  2. Text preprocessing  ###################################
+##################################################################################
+
+#sent_tokenize() : Sentence tokenization
+
+################ Removing punctuation #######################
+def remove_punctuation(text):
+    '''a function for removing punctuation'''
+    import string
+    # replacing the punctuations with no space, 
+    # which in effect deletes the punctuation marks 
+    translator = str.maketrans('', '', string.punctuation)
+    # return the text stripped of punctuation marks
+    return text.translate(translator)
+
+df['texte_nopuk'] = df['texte'].apply(remove_punctuation)
+
+################## Lowering cases in all articles ##########################
+#df['texte']=pd.Series(df['texte']).str.lower()
+
+#################### Tokenization and remove punctuation: ##################
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer(r'\w+')
+
+df['tokens']=df['titre']
+for i in range(0,len(df)):
+    df['tokens'][i] = tokenizer.tokenize(df['texte'][i])
+
+#print(df['tokens'][0])
+
+###################### Remove stopwords (a, at, the..) #####################
+from nltk.corpus import stopwords
+sw = stopwords.words('french')
+
+sw.append('a')
+
+#Option 2:        
+def stopwords(text):
+    # removing the stop words and lowercasing the selected words
+    text = [word for word in text if word.lower() not in sw]
+    # joining the list of words with space separator
+    return " ".join(text)
+
+df['no_sw'] = df['tokens'].apply(stopwords)    
+
+#df['no_sw'][0]
+
+df['tokens_no_sw']= df['no_sw']
+
+# Tokenization:
+for i in range(0,len(df)):
+    df['tokens_no_sw'][i] = [t for t in df['no_sw'][i].split()]
+
+#df['tokens_no_sw'][0]
+
+########################## SAME ROOTS WORDS ###################################
+# Stemming together words of same roots:
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("french")
+
+def stemming(text):    
+    '''a function which stems each word in the given text'''
+    text = [stemmer.stem(word) for word in text.split()]
+    return " ".join(text) 
+
+df['same_roots'] = df['no_sw'].apply(stemming)
+
+#df['same_roots'][0]
+
+# Tokenization of same_roots text:
+df['same_roots_tk']=df['same_roots']
+for i in range(0,len(df)):
+    df['same_roots_tk'][i] = [t for t in df['same_roots'][i].split()]
+
+#df['same_roots_tk'][0]
+
+##################################################################################
+########################## 3. Text analysis  #####################################
+##################################################################################
+
+########################## COUNT WORDS ########################################
+
+def count_words(i,text):
+    " Compte l'occurence d'un mot dans l'article i"
+    freq = nltk.FreqDist(df[text][i])
+    for key,val in freq.items():
+        print(str(key) + ':' + str(val))
+    freq.plot(20, cumulative=False)
+
+count_words(0,text='tokens_no_sw')
+count_words(0,text='same_roots_tk')
+
+
+########################## IDENTIFICATION #####################################
+
+def return_POS(sentence):
+    # Tokeniser la phrase
+    doc = nlp(sentence)
+    # Retourner les étiquettes de chaque token
+    return [(X, X.pos_) for X in doc]
+
+def ident_wordtype(text):
+    noun = pd.DataFrame(nltk.pos_tag(df[text][0]))
+    cl_np=[]
+    for i in range(0,len(noun)):
+        if noun[1][i]=='NNP':
+               cl_np.append(noun[0][i]) 
+    return noun
+
+token_type=ident_wordtype('tokens')
+ident_wordtype('no_puk_sw_tk')
+
+import fr_core_news_sm
+
+#from pprint import pprint
+nlp = fr_core_news_sm.load()
+
+#Possible types:
+#PERSON	People, including fictional.
+#NORP	Nationalities or religious or political groups.
+#FAC	Buildings, airports, highways, bridges, etc.
+#ORG	Companies, agencies, institutions, etc.
+#GPE	Countries, cities, states.
+#LOC	Non-GPE locations, mountain ranges, bodies of water.
+#PRODUCT	Objects, vehicles, foods, etc. (Not services.)
+#EVENT	Named hurricanes, battles, wars, sports events, etc.
+#WORK_OF_ART	Titles of books, songs, etc.
+#LAW	Named documents made into laws.
+#LANGUAGE	Any named language.
+#DATE	Absolute or relative dates or periods.
+#TIME	Times smaller than a day.
+#PERCENT	Percentage, including ”%“.
+#MONEY	Monetary values, including unit.
+#QUANTITY	Measurements, as of weight or distance.
+#ORDINAL	“first”, “second”, etc.
+#CARDINAL	Numerals that do not fall under another type.
+df['org']=df['titre']
+
+def ident_nom_propre(i,text):
+    """Identifie les chiffres,dates et noms propres. Les organisations seront identifier a l'article"""
+    doc = nlp(df[text][i])
+    #pprint([(X.text, X.label_) for X in doc.ents])
+    ident=[(X.text, X.label_) for X in doc.ents]
+#    id2=pd.DataFrame(ident, columns=['mots','type'])    
+    org=[]
+    for j in range(0,len(ident)):
+        if ident[j][1]=='ORG':
+              org.append(ident[j][0])
+    df['org'][i]=org
+#    return id2
+
+#On va lancer la fonction pour tout article:
+for art in range(0,len(df)):
+    ident_nom_propre(art,'no_sw')
+    
+
+df['ident']='Non'
+       
+def ident_org(i):
+    """ Identifie si une entreprise du groupe LVMH est dans l'article """
+    for i in range(len(df)):
+        check =  any(item in df['org'][i] for item in org_lvmh)
+        if check is True:
+            df['ident'][i]='Oui'
+            
+import time
+start = time.time()                  
+for i in range(len(df)):                             
+    ident_org(i)
+end = time.time()
+print(round(end - start),'secondes')
+
+########################################################################################
+########################## EXPORTATION DES DF TRAITES #################################
+########################################################################################
+
+########################### TOUS LES ARTICLES TRAITES #############################
+path=r"C:\Users\jovan\OneDrive\Radna površina\Paris 1\NLP_2\data\le_parisien\le_parisien_final.csv"
+df.to_csv(path, index = False, header=True)
+
+#Creer une df avec uniquement les articles d'economies
+#df_eco=df[df['categorie']=='economie']
+
+#Creer une dataframe avec uniquement les articles en lien avec LVMH
+#df_lvmh=df[df['ident']=='Oui']    
+
+########################## ARTICLES POSITIFS ET NEGATIFS IDENTIFIES ####################
+
+#Manuellement:   
+pos=[1,2,4,7,9,10,12,16,23,24,25,26,27,28,29,32,34,35,36,37,38,41,43,45,48,51,54,55,57,
+41,63,64,65,66,67,68,74,76,78,80,84,85,91,96,97,104,112,113,117,119,120,123,134,136,139,146,
+153,165,168,169,170,222,243,480,544,554,640,649,651,1749,
+2217,2465,4420,4429,4440,4493,4633,4767,4980,4983,5107,6733,6797,6806,6945,6949]
+
+
+
+neg=[0,3,5,6,8,11,13,14,15,17,18,19,20,21,22,30,31,33,39,40,42,44,46,47,49,50,52,53,56,58,59,
+     60,61,62,69,70,71,72,73,75,77,79,81,82,83,86,87,88,89,90,92,93,94,95,98,99,
+     100,101,102,103,105,107,108,109,115,116,121,125,126,127,128,135,137,138,139,140,141,148,
+     156,158,164,169,
+     784,761,
+     1065,1270,1632,1994,
+     2014,2055,2210,2359,2636,3650,3660,4210,4216,4485,4549,4660
+     ,6006,6665,6671,6721,6792,6888,6898,9840]
+
+for i in range(126,140):
+    if i not in pos:
+        if i not in neg:
+            print(i)
+    if df['categorie'][i] in ['societe','international','politique''culture-loisirs', 'economie','environnement','high-tech', 'sciences', 'immobilier']:
+            print(i,df['lien'][i])
+        
+
+#df['categorie'].unique()
+df['texte'][172]
+
+df_pos=df.loc[pos]
+df_neg=df.loc[neg]
+
+path2=r"C:\Users\jovan\OneDrive\Radna površina\Paris 1\NLP_2\data\le_parisien\le_parisien_pos.csv"
+path3=r"C:\Users\jovan\OneDrive\Radna površina\Paris 1\NLP_2\data\le_parisien\le_parisien_neg.csv"
+
+df_pos.to_csv(path2, index = False, header=True)
+df_neg.to_csv(path3, index = False, header=True)
+
+all_av=pos+neg
+df_avis=df.loc[all_av]
+df_avis.reset_index(drop=True,inplace=True)
+######################################################################################
+######################## PREPROCESSING BAYES NLTK ####################################
+######################################################################################
+# Dure longtemps et est lancer une seule fois
+from nltk.tokenize import word_tokenize
+
+def bayes_prep(text):
+    data=([(posi[text], 'positive') for index, posi in df_pos.iterrows()]+
+           [(nega[text], 'negative') for index, nega in df_neg.iterrows()])
+    # df.iterrows() : Iterate over df rows. Returns index of the row in DataFrame (tuple when multiple idx).
+    # Then returns the row data as Pandas Series. row is like the df[i] iterated i times.
+    
+    # Create a set of tokenized words. Set items are unordered, no duplicates and immutable 
+    # set elements. Any type or number of elements (except lists and dictionnaries).
+    tokens=list(word.lower() for words in data for word in word_tokenize(words[0]))
+    tokens2 = [item.replace("'","") for item in tokens]
+    tokens3 = [x for x in tokens2 if x]  
+    tokenss = []
+    [tokenss.append(x) for x in tokens3 if x not in tokenss]
+    tokens_set=set(tokenss)
+    #    print(tokens_set)
+    #Donc on va creer un ensemble de mots apparaissant sur l'ensemble des articles
+    
+    # Pour chaque article de data, on va prendre son label positive/negatif x[1]. 
+    # On va aussi tokenizer les mots de x[0] et mettre un label true/false si le mot est dans
+    # l'article ou pas.
+    train=[({word:(word in word_tokenize(x[0])) for word in tokens_set}, x[1]) for x in data]
+    return train
+
+txt=['texte', 'no_sw', 'same_roots', 'texte_nopuk']
+
+train_texte=bayes_prep('texte')
+train_no_sw=bayes_prep('no_sw')
+train_same_roots=bayes_prep('same_roots')
+train_texte_nopuk=bayes_prep('texte_nopuk')
+
+#df_avis=pd.read_csv('C:/Users/jovan/OneDrive/Radna površina/Paris 1/NLP_2/data/le_parisien/le_parisien_avis.csv')
+
+df_texte = pd.DataFrame(train_texte, columns =['Label_texte', 'Avis'])
+df_no_sw = pd.DataFrame(train_no_sw, columns =['Label_no_sw', 'Avis'])
+df_same_roots = pd.DataFrame(train_no_sw, columns =['Label_same_roots', 'Avis'])
+df_texte_nopuk = pd.DataFrame(train_no_sw, columns =['Label_no_puk', 'Avis'])
+
+df_avis['Label_texte'] = df_texte['Label_texte']
+df_avis['Avis'] = df_texte['Avis']
+df_avis['Label_no_sw'] = df_no_sw['Label_no_sw']
+df_avis['Label_same_roots'] = df_same_roots['Label_same_roots']
+df_avis['Label_no_puk'] = df_texte_nopuk['Label_no_puk']
+
+
+path1=r"C:\Users\jovan\OneDrive\Radna površina\Paris 1\NLP_2\data\le_parisien\le_parisien_avis.csv"
+df_avis.to_csv(path1, index=False, header=True)
---
+++ Interface/Le Parisien/Naive_Bayes_Parisien.py
@@ -0,0 +1,215 @@
+import pandas as pd
+import nltk
+#nltk.download()
+
+##################################################################################
+###################### IMPORTATION DE DONNEES  ###################################
+##################################################################################
+df=pd.read_csv('C:/Users/jovan/OneDrive/Radna površina/Paris 1/NLP_2/data/le_parisien/le_parisien_final.csv')
+
+
+# Correction manuelle d'un article non-liée à LVMH
+df['ident'][1112]='Non'
+#df['ident'].value_counts()
+#12 articles sur le groupe LVMH
+
+df_avis=pd.read_csv('C:/Users/jovan/OneDrive/Radna površina/Paris 1/NLP_2/data/le_parisien/le_parisien_avis.csv')
+
+#df_avis['Label_texte'][2][:110]
+
+
+##################################################################################
+#########################   NAIVE BAYES ESTIMATOR  ###############################
+##################################################################################
+
+################## CREATION LISTE POUR CHAQUE TYPE DE TEXTE ######################
+
+#tx=['Label_texte','Label_no_sw','Label_same_roots','Label_no_puk']
+def train(txt):
+    t=[]  
+    t1=df_avis[txt].tolist()
+    t2=df_avis['Avis'].tolist()     
+    import ast
+    for i in range(len(t1)):
+        t.append((ast.literal_eval(t1[i]),t2[i]))
+    return t
+
+input_texte=train('Label_texte')
+input_no_sw=train('Label_no_sw')
+input_same_roots=train('Label_same_roots')
+input_no_puk=train('Label_no_puk')
+
+
+############################# FONCTION CREATRICE DU BAYESIEN #########################
+import random
+#from sklearn.model_selection import KFold
+#0.9*len(input_no_puk)
+def bayes(input_tx,n=100,p=0.8):
+    """ Il faut introduire dans la fonction le type de texte input_tx et le nombre de 
+        repetitions n. p sera le pourcentage des données dédiés au train.  """
+    all_acc=[]
+    proba=[]
+    for i in range(n):
+        random.shuffle(input_tx) # va changer l'ordre dans train ce qui va permettre d'avoir un 
+        # nouveau train et test ci-dessous.
+        pourc=round(p*len(df_avis))
+        train_x=input_tx[0:pourc]
+        test_x=input_tx[pourc:len(input_tx)]
+        
+        model = nltk.NaiveBayesClassifier.train(train_x)
+        acc=nltk.classify.accuracy(model, test_x)
+#        print(model.classify(features_all('bien')))
+#        print(model.labels())
+#        print("Accuracy:", acc)
+        all_acc.append(acc)
+    for j in range(0,len(test_x)):
+        prob_dist = model.prob_classify(test_x[j][0])
+        proba.append([prob_dist.prob("positive"),prob_dist.prob("negative")])
+#        print("positive " + str(prob_dist.prob("positive")))
+#        print("negative " + str(prob_dist.prob("negative")))
+    model.show_most_informative_features(n=20)
+#    print('On a une précision de', sum(all_acc)/n)
+    return all_acc,proba
+
+###################### ENTRAINEMENT SUR DIFFERENTS TYPES DE TEXTES #######################
+
+#Pour chaque type de texte, serait calculé dans l'odrde suivant: 
+# - la precision sur 100 iterations
+# - la probabilite associe aux decisions pour le derniere iteration
+# - la moyenne et l'ecart type de toutes les iterations
+
+############## Avec le texte brut ###################
+
+acc_texte,proba_texte=bayes(input_texte,100,p=0.8)  
+
+moy_txt=sum(acc_texte)/len(acc_texte)
+std_txt=sum((i - moy_txt) ** 2 for i in acc_texte) / len(acc_texte) 
+print('La moyenne est:',moy_txt,"et l'ecart-type est:",std_txt)
+
+############ Avec la ponctuation elimines ##########################
+
+acc_no_sw,proba_sw=bayes(input_no_sw,100,p=0.85)
+
+moy_no_sw=sum(acc_no_sw)/len(acc_no_sw)
+std_no_sw=sum((i - moy_no_sw) ** 2 for i in acc_no_sw) / len(acc_no_sw) 
+print('La moyenne est:',moy_no_sw,"et l'ecart-type est:",std_no_sw)
+
+############ Avec la source du mot ###################
+
+acc_same_roots,proba_same_roots=bayes(input_same_roots,1000)
+
+moy_same_roots=sum(acc_same_roots)/len(acc_same_roots)
+std_same_roots=sum((i - moy_same_roots) ** 2 for i in acc_same_roots) / len(acc_same_roots) 
+print('La moyenne est:',moy_same_roots,"et l'ecart-type est:",std_same_roots)
+
+############ Avec les stopwords et la ponctuation elimines ############
+
+acc_no_puk,proba_no_puk=bayes(input_no_puk,1000)
+
+moy_no_puk=sum(acc_no_puk)/len(acc_no_puk)
+std_no_puk=sum((i - moy_no_puk) ** 2 for i in acc_no_puk) / len(acc_no_puk) 
+print('La moyenne est:',moy_no_puk,"et l'ecart-type est:",std_no_puk)
+
+########################### Matrice de confusion #######################################
+from collections import defaultdict
+refsets = defaultdict(set)
+testsets = defaultdict(set)
+labels = []
+tests = []
+random.shuffle(input_texte)
+model = nltk.NaiveBayesClassifier.train(input_texte[:119])
+for i, (feats, label) in enumerate(input_texte[120:158]):
+    refsets[label].add(i)
+    observed = model.classify(feats)
+    testsets[observed].add(i)
+    labels.append(label)
+    tests.append(observed)
+
+print(nltk.ConfusionMatrix(labels, tests))
+
+############################################################################################
+############################### DESCRIPTIF STAT ############################################
+###########################################################################################
+
+####################### Frequence d'un mot dans un article ##############################
+from nltk.probability import FreqDist
+
+mots=list(df['tokens'])
+l=mots[0].strip('][').split(', ')
+
+fdist = FreqDist(l)
+print(fdist)
+fdist.most_common(5)
+
+# Frequency Distribution Plot
+import matplotlib.pyplot as plt
+fdist.plot(30,cumulative=False)
+plt.show()
+
+
+######################### AVIS DE LA PHRASE OU DU MOT ###################################
+
+# Une manière intéressante de tester le fonctionnement de notre bayesien est de lui faire 
+# rentrer une phrase, un mot ou une expression et voir le sentiment qu'il attribue.
+# Par exemple, "Bien" a un avis positif et "Mal" a un avis négatif.
+
+def bayes_avis(input_tx,string):
+    """ Pour une phrase quelconque retourne l'avis selon l'estimateur. """
+    model = nltk.NaiveBayesClassifier.train(input_tx)
+#    model.show_most_informative_features(15)
+    exemple = string 
+    t_features = {c:True for c in exemple.lower().split()}
+    print(exemple," : ", model.classify(t_features)) 
+    print("Probabilité sentiment positif: " + str(model.prob_classify(t_features).prob("positive")))
+    print("Probabilité sentiment négatif: " + str(model.prob_classify(t_features).prob("negative")))
+
+bayes_avis(input_no_sw,"Bien")
+#Avis positif
+
+bayes_avis(input_no_sw,"Mal")
+#Avis négatif
+
+bayes_avis(input_no_sw,"Pas si bien que ça")
+# Ce genre d'expression montre la limite de notre classificateur. Il est plus dur d'associer
+# des groupes de mots de signification parfois contre-intuitive lorsqu'on regarde les mots
+# individuellement. C'est le cas d'expression ironique, d'oxymore... 
+
+
+#########################################################################################
+############################ Application de notre algorithme ############################
+##########################################################################################
+def bayes_appli(input_tx,tests):
+    """ La fonction va classifier un article en positif ou negatif. Il faut choisir le
+        type de texte traité. """
+    all_ft=[]
+    model = nltk.NaiveBayesClassifier.train(input_tx)
+    res=[]
+    for test in tests:
+     t_features = {c:True for c in test.lower().split()}
+     print(test," : ", model.classify(t_features)) 
+     all_ft.append(model.classify(t_features))
+     res.append([tests,model.classify(t_features)])
+    return all_ft,res
+
+# Nous allons appliquer notre fonction aux entreprise liées au groupe LVMH, pour en déduire
+# si il reçoivent un traitement privilégier du journal Parisien:
+
+tests_lvmh=df['texte'][df['ident']=='Oui'].tolist()
+
+avis,res=bayes_appli(input_no_sw,tests_lvmh)
+
+import pandas as pd 
+pd.Series(avis).value_counts()
+
+# Selon notre classificateur, des 12 articles sur les entreprises du groupe LVMH, 9 sont
+# positifs et que 3 sont négatifs.
+# Selon nous, après avoir observé les 12 articles, l'algorithme a fait une classification
+#sans faille des articles.
+# 1 article négatif et 1 article positif ne sont que des reportages de Radio Classique du 
+#groupe LVMH, donc pas directement lié à l'image du groupe, mais bien classé. 2 articles 
+#raportent des faits négatifs sur le groupe, touchant à des cas de poursuite judiciaire 
+#sans fin. Finalement, tout les autres articles sont positifs, de manière assez prononcé, 
+# et parlant d'action bénéfique du groupe sur la société.
+ 
+# Dans cette exemple précis, notre classificateur a montré sa force en classifiant 
+# parfaitement les articles.
